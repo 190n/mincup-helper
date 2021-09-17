b2a71e6e622e9b429636bc167c7a6044b487d7db
Ben Grant <benjamin.j.grant@gmail.com>
2021-09-17 09:23:34 -0700

    Twitter API testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,29 @@
 import type { NextPage } from 'next';
 
-const Home: NextPage = () => (
-    <h1 className="title is-1">MinCup Helper</h1>
+const Home: NextPage<{ data: any[] }> = (props) => (
+    <>
+        <h1 className="title is-1">MinCup Helper</h1>
+        {props.data.map((tweet, i) => (
+            <div className="card block" key={i}>
+                <div className="card-content">
+                    {tweet.text}
+                </div>
+            </div>
+        ))}
+    </>
 );
 
 export default Home;
+
+export async function getStaticProps() {
+    const res = await fetch('https://api.twitter.com/2/tweets/search/recent?query=%23Torbernite', {
+        headers: {
+            Authorization: `Bearer ${process.env.TWITTER_TOKEN}`,
+        },
+    });
+    const data = await res.json();
+
+    return {
+        props: data,
+    };
+}
